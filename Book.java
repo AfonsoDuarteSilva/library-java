16f1a2b9edc73388d938e1c202765109e58b6d9e
Afonso <afonsorsilva07@gmail.com>
2026-03-10 22:42:07 +0000

    Add Library class with borrowBook, returnBook and searchLoan methods and fixed some javadoc

--- Book.java
+++ Book.java
@@ -1,24 +1,69 @@
+/**
+ * Represents a book 
+ */
 public class Book {
-    private String title;
-    private String author;
-    private String ISBN;
-    private String category;
+    private final String title;
+    private final String author;
+    private final String ISBN;
+    private final String genre;
     private boolean available;
 
     /**
-     * 
+     * Creates a new book with the given information
+     * @param title the book´s title
+     * @param author the book´s author
+     * @param ISBN the book´s unique id
+     * @param genre the book´s genre
      */
     public Book (String title, String author, String ISBN, String category) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
-        this.category = category;
+        this.genre = category;
         available = true;
 
     }
 
+    /**
+     * Gives the title of the book
+     */
     public String getTitle() {
         return this.title;
     }
-    
+
+    /**
+     * Gives the author of the book
+     */
+    public String getAuthor() {
+        return this.author;
+    }
+
+    /**
+     * Gives the book unique id
+     */
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    /**
+     * Gives the book genre
+     */
+    public String getGenre() {
+        return this.genre;
+    }
+
+    /**
+     * Tell´s wether or not the book is available for borrowing
+     */
+    public boolean isAvailable() {
+        return this.available;
+    }
+
+    /**
+     * Sets the book availability
+     * @param available whether the book is available for borrowing
+     */
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
--- Library.java
+++ Library.java
@@ -1,5 +1,67 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 public class Library {
     private ArrayList<Book> books;
     private ArrayList<User> users;
+    private ArrayList<Loan> onLoanBooks;
+
+    /**
+     * Creates a new library
+     */
+    public Library () {
+        this.books = new ArrayList<>();
+        this.users = new ArrayList<>();
+        this.onLoanBooks = new ArrayList<>();
+    }
+
+    /**
+     * Adds a book to the library
+     * @param book book to add
+     */
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    /**
+     * Adds a user to the library
+     * @param user user to add
+     */
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    /**
+     * Borrows a book that the user asked
+     * @param user that asked to borrow the book
+     * @param book book  that the user asked to borrow
+     */
+    public void borrowBook(User user, Book book) {
+        if (book.isAvailable()) {
+            Loan bookOnLoan = new Loan(book, user, LocalDate.now());
+            onLoanBooks.add(bookOnLoan);
+            book.setAvailable(false);
+        } else {
+            throw new IllegalArgumentException("Book is not available to borrow");
+        }
+    }
+
+    /**
+     * Returns a book on loan to the library
+     * @param user user that returned the book
+     * @param book book that was returned
+     * @requires {@code !book.isAvailable()}
+     */
+    public void returnBook(User user, Book book) {
+        onLoanBooks.remove(searchLoan(book));
+        book.setAvailable(true);
+    }
+
+    public Loan searchLoan(Book book) {
+        for(Loan loan : onLoanBooks) {
+            if(loan.getRequestedBook().equals(book)) {
+                return loan;
+            } 
+        }
+        throw new IllegalArgumentException("Book not found");
+    }
 }
